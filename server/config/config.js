512be552865b43bed20baac00310f2cd96a93039
Adnan Latif <alatif.bwp@gmail.com>
2025-04-26 02:51:12 +0500

    Added seeders and some other tweaks.

--- server/config/config.js
+++ server/config/config.js
@@ -1,23 +1,28 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
+
+const dialect = process.env.DB_DIALECT || 'mysql';
+
 module.exports = {
   development: {
-    username: 'root',
-    password: 'password',
-    database: 'traffic_db',
-    host: '127.0.0.1',
-    dialect: 'mysql',
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'traffic_db',
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: dialect,
   },
   test: {
-    username: 'root',
-    password: 'password',
-    database: 'traffic_db_test',
-    host: '127.0.0.1',
-    dialect: 'mysql',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: `${process.env.DB_NAME}_test`,
+    host: process.env.DB_HOST,
+    dialect: dialect,
   },
   production: {
-    username: 'root',
-    password: 'password',
-    database: 'traffic_db_prod',
-    host: '127.0.0.1',
-    dialect: 'mysql',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: `${process.env.DB_NAME}_prod`,
+    host: process.env.DB_HOST,
+    dialect: dialect,
   },
 };
