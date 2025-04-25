097793b57b712d3a30eb077b0e77622d946c389f
Adnan Latif <alatif.bwp@gmail.com>
2025-04-26 03:45:43 +0500

    Updated path.

--- server/models/index.js
+++ server/models/index.js
@@ -5,8 +5,10 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
+
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(path.resolve(__dirname, '../config/config.js'))[env];
 const db = {};
 
 let sequelize;
